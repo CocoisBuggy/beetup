58d463de8e00c63c98b4cd9ab73f7663bd0cb82d
CocoIsBuggy <g.colinharley@gmail.com>
2025-11-30 11:34:31 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            android_sdk.accept_license = true;
+          };
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "android-compose-dev";
+
+          packages = with pkgs; [
+            jdk21
+            android-studio
+            gradle
+            git
+          ];
+
+          shellHook = ''
+            export JAVA_HOME=${pkgs.jdk21}/lib/openjdk
+          '';
+        };
+      }
+    );
+}
